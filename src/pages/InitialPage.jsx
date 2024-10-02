ca26294e32265340cde371ae1380766d1c0f106b
aly <alyreinert@gmail.com>
2024-10-01 22:14:11 -0300

    deploy with initial page

--- src/pages/InitialPage.jsx
+++ src/pages/InitialPage.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import Title from "../components/Title"
 import Subtitle from "../components/Subtitle"
-import ClientView from "../components/Clientview"
-import ClientsContext from "../contexts/ClientsContext";
-import { useContext } from "react";
+import ClientView from "../components/ClientView"
 
 function InitialPage() {
-    const {clients, updateClients} = useContext(ClientsContext);
     return (
     <div className='flex justify-center bg-slate-200 min-w-80 rounded-md p-2'>
         <div className="max-w-2xl min-w-80 space-y-1">
             <Title>Meu Cliente</Title>
             <Subtitle>Fluxo pré-venda</Subtitle>
-            <ClientView/>
+            <ClientView></ClientView>
         </div>
     </div>
     )
